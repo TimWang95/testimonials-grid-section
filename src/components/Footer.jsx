0c32cc1d334338bf8d07915bb95a670951f4f262
TimWang95 <tgm761@gmail.com>
2023-05-02 20:48:24 +0800

    add challenge

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <>
+      <footer className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>.
+        Coded by <a href="https://www.frontendmentor.io/profile/TimWang95">Tim Wang</a>.
+      </footer>
+    </>
+  )
+}
